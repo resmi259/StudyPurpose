b19412051b1ec3b55f034d4ce4c5f520d9d8174b
resmi.nair <resmi.nair@FLY-LP1054.flytxt.com>
2025-02-01 14:46:13 +0530

    AddedcollectionclassFirstCommt

---
+++ GITStudyProject/src/main/java/CollectionStudy.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionStudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	Map<String,Integer> student = new HashMap<>();
+		
+		
+		student.put("ABC", 99);
+		
+		student.put("ABC1", 100);
+		student.put("ABC2", 150);
+		student.put("ABC3", 80);
+		
+		System.out.println(student.keySet());
+		for (String key:student.keySet() )
+			
+		{
+			System.out.println(key + ":" + student.get(key) );
+		}
+		
+		
+
+	}
+
+}
